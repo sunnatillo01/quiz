30aaed63a1fb51f9a10dfd35ee98913d6c352ace
sunnatillo0107@gmail.com <sunnatillo0107@gmail.com>
2023-01-20 12:04:43 -0800

    commit

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -19,40 +19,123 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text('Categoriya 1'),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Center(
-              child: InkWell(
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return const WelcomeScreen();
-                      },
-                    ),
-                  );
-                },
-                child: Container(
-                  decoration: const BoxDecoration(),
-                  width: w,
-                  height: h,
-                  child: const Image(
-                    image: AssetImage(
-                      'assets/images/1.jpg',
-                    ),
-                    fit: BoxFit.cover,
-                  ),
+            Container(
+              height: 148.0,
+              width: 266.0,
+              child: const Image(
+                image: AssetImage(
+                  "assets/images/2.jpg",
                 ),
               ),
             ),
+           const  SizedBox(height: 10),
+           Container(
+            child: const Text('Menu',style: TextStyle(fontSize: 18,fontWeight: FontWeight.w700),),
+           ),
+           const SizedBox(height: 10),
+           Row(
+             children: [
+               Padding(
+                 padding: const EdgeInsets.all(6.0),
+                 child: Column(
+                   children: [
+                     Container(
+                      height: 70.0,
+                      width: 73.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: const Color(0xFFECF0F1),
+                      ),
+                       child: const Image(
+                image: AssetImage(
+                  "assets/images/4.jpg",
+                ),
+              ),
+                     ),
+                     const SizedBox(height: 10),
+                    const Text('Quiz',style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400),),
+                   ],
+                 ),
+               ),
+               Padding(
+                 padding: const EdgeInsets.all(6.0),
+                 child: Column(
+                   children: [
+                     Container(
+                      height: 70.0,
+                      width: 73.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: const Color(0xFFD35400),
+                      ),
+                       child: const Image(
+                image: AssetImage(
+                  "assets/images/4.jpg",
+                ),
+              ),
+                     ),
+                      const SizedBox(height: 10),
+                    const Text('Quiz',style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400),),
+                   ],
+                 ),
+               ),
+               Padding(
+                 padding: const EdgeInsets.all(6.0),
+                 child: Column(
+                   children: [
+                     Container(
+                      height: 70.0,
+                      width: 73.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: const Color(0xFFECF0F1),
+                      ),
+                       child: const Image(
+                image: AssetImage(
+                  "assets/images/5.jpg",
+                ),
+              ),
+                     ),
+                      const SizedBox(height: 10),
+                    const Text('Quiz',style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400),),
+                   ],
+                 ),
+               ),
+               Padding(
+                 padding: const EdgeInsets.all(6.0),
+                 child: Column(
+                   children: [
+                     Container(
+                      height: 70.0,
+                      width: 73.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: const Color(0xFFECF0F1),
+                      ),
+                       child: const Image(
+                image: AssetImage(
+                  "assets/images/6.jpg",
+                ),
+              ),
+                     ),
+                      const SizedBox(height: 10),
+                    const Text('Quiz',style: TextStyle(fontSize: 14,fontWeight: FontWeight.w400),),
+                   ],
+                 ),
+               ),
+             ],
+           ),
+            const  SizedBox(height: 10.0),
+           Container(
+            child: const Text('Quiz Category',style: TextStyle(fontSize: 18,fontWeight: FontWeight.w700),),
+           ),
           ],
         ),
       ),
+      
     );
   }
 }
